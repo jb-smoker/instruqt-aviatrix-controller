c794722614cc74d5246f1d3341acb7bc781758cc
John Smoker <jsmoker@aviatrix.com>
2022-03-16 13:09:43 -0700

    byol

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "vpc" {
   name = "avx-controller-vpc"
   cidr = var.cidr
 
-  azs            = ["us-west-2"]
+  azs            = ["us-west-2a"]
   public_subnets = [cidrsubnet(var.cidr, 8, 1)]
 
   enable_nat_gateway = false
@@ -31,14 +31,15 @@ resource "aws_key_pair" "aviatrix_controller" {
 }
 
 module "aviatrix_iam_roles" {
-  source = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-iam-roles?ref=terraform_0.14"
+  source = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-iam-roles?ref=9137282819944a79e56d3bb5977acf14fb5d6048"
 }
 
 module "aviatrix_controller" {
-  source            = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-build?ref=terraform_0.14"
+  source            = "github.com/AviatrixSystems/terraform-modules.git//aviatrix-controller-build?ref=350883d4d3a07f4fa60ab9a40ac3bcec92af5e7a"
   vpc               = module.vpc.vpc_id
+  type              = "BYOL" # "MeteredPlatinumCopilot"
   subnet            = module.vpc.public_subnets[0]
   keypair           = aws_key_pair.aviatrix_controller.key_name
   ec2role           = module.aviatrix_iam_roles.aviatrix-role-ec2-name
-  incoming_ssl_cidr = var.cidr
+  incoming_ssl_cidr = [var.cidr]
 }

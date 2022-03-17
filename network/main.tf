24ee6b8ac5800255b93bb415bc508cc4bbadcddd
John Smoker <jsmoker@aviatrix.com>
2022-03-17 10:46:04 -0700

    duplicate providers

--- network/main.tf
+++ network/main.tf
@@ -10,17 +10,6 @@ provider "aviatrix" {
   password      = var.aviatrix_password
 }
 
-terraform {
-  required_providers {
-    aviatrix = {
-      source  = "aviatrixsystems/aviatrix"
-      version = "2.21.0-6.6.ga"
-    }
-  }
-  required_version = ">= 1.0.0"
-}
-
-
 # --------------------------------------------
 
 # aviatrix student user
